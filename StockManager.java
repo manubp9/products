0b8a6b72eba23b8b89f786d2886a34ba217808a5
Manuel <manubp9@gmail.com>
2016-02-19 11:08:44 +0100

    corregido metodo addProduct para que solo añada un objeto si no existe otro con el mismo id  en la lista

--- StockManager.java
+++ StockManager.java
@@ -26,7 +26,11 @@ public class StockManager
      */
     public void addProduct(Product item)
     {
-        stock.add(item);
+        if(findProduct(item.getID()) == null)
+        {
+            stock.add(item);
+        }
+        
     }
 
     /**
